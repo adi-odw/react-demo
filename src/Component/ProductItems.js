f879a60fc8334c0414fc02a48b0286ae69df16c3
adi-odw <aditya@odw.rocks>
2021-03-26 14:04:07 +0530

    increament and decreament added

--- src/Component/ProductItems.js
+++ src/Component/ProductItems.js
@@ -4,16 +4,32 @@ import './ProductItems.css';
 export default class ProductItems extends React.Component {
     state = {
         loading: true,
-        product: []
+        product: [],
+        clicks: 0,
+        show: true
     };
 
     async componentDidMount() {
         const url = `https://fakestoreapi.com/products/${this.props.match.params.id}`;
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        //console.log(data);
         this.setState({ product: data, loading: false });
 
+    }
+
+    IncrementItem = () => {
+        this.setState({ clicks: this.state.clicks + 1 });
+    }
+    DecreaseItem = () => {
+        if (this.state.clicks > 0) {
+
+            this.setState({ clicks: this.state.clicks - 1 });
+
+        }
+    }
+    ToggleClick = () => {
+        this.setState({ show: !this.state.show });
     }
 
     render() {
@@ -37,9 +53,17 @@ export default class ProductItems extends React.Component {
                                 <div>Category: {this.state.product.category}</div>
                                 <div><b>Price:{this.state.product.price}</b></div>
                                 <br></br>
-                                <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value">-</div>
-                                <input type="number" id="number" value="0" />
-                                <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value">+</div>
+                                <div class="value-button" onClick={this.DecreaseItem} value="Decrease Value">-</div>
+                                <input type="number" value={this.state.clicks} readOnly class="inputProductNo" />
+                                <div class="value-button" onClick={this.IncrementItem} value="Increase Value">+</div>
+                                {/* <div>
+                                    <button onClick={this.IncrementItem}>Click to increment by 1</button>
+                                    <button onClick={this.DecreaseItem}>Click to decrease by 1</button>
+                                    <button onClick={this.ToggleClick}>
+                                        {this.state.show ? 'Hide number' : 'Show number'}
+                                    </button>
+                                    {this.state.show ? <h2>{this.state.clicks}</h2> : ''}
+                                </div> */}
                                 <button>Add to Cart</button>
                             </div>
                         </div>
